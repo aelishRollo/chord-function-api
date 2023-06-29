a5517c38020782e3c2fe800bb95669729df28dd4
Aelish <alecrollison@icloud.com>
2023-06-28 22:32:10 -0400

    Refactor test file into seperate suites

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const {parseChordName,removeDuplicateStringsFromArray} = require('./index');       //import the index MODULE a.k.a. file, not necessrily function. The index module just happens to import the sum function
 
 
-describe('Here are my tests', () => {
+describe('parseChordName()', () => {
   test('parses "cmajor7b9#11" correctly', () => {
     const result = parseChordName("cmajor7b9#11");
     expect(result).toEqual(['c', 'major7', 'b9#11']);
@@ -10,6 +10,9 @@ describe('Here are my tests', () => {
     const result = parseChordName("abminor7#6");
     expect(result).toEqual(['ab', 'minor7', '#6']);
   });
+});
+
+describe('removeDuplicateStringsFromArray', () => {
   test('Removes duplicate strings from array', () => {
     const result = removeDuplicateStringsFromArray(['a','b','c','c'])
     expect(result).toEqual(['a','b','c']);
