925597cf70a5feb70d3897f0303255d7f3faaa63
Aelish <alecrollison@icloud.com>
2023-06-28 22:25:28 -0400

    Add "abminor7#6" test for parseChordName()

--- index.test.js
+++ index.test.js
@@ -1,10 +1,14 @@
 const {parseChordName,removeDuplicateStringsFromArray} = require('./index');       //import the index MODULE a.k.a. file, not necessrily function. The index module just happens to import the sum function
 
 
-describe('Here are my tests hombre', () => {
+describe('Here are my tests', () => {
   test('parses "cmajor7b9#11" correctly', () => {
     const result = parseChordName("cmajor7b9#11");
     expect(result).toEqual(['c', 'major7', 'b9#11']);
+  });
+  test('parses "abminor7#6" correctly', () => {
+    const result = parseChordName("abminor7#6");
+    expect(result).toEqual(['ab', 'minor7', '#6']);
   });
   test('Removes duplicate strings from array', () => {
     const result = removeDuplicateStringsFromArray(['a','b','c','c'])
