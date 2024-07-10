ce8b48fd5100ea6472ad1a2025667c8fd76ec931
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-07-10 16:46:37 -0400

    Update index.html and serve an object at /api

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+
+let myObject = {
+    name: 'Prometheus',
+    age: 9001,
+    bipedal: true
+}
+
+
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
     console.log('You are at the main route!')
+})
+
+app.get('/api', (req,res) => {
+    res.json(myObject)
+    console.log('User made a request, yo')
 })
 
 
