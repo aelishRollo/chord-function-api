ad6b083bb51e593a1fabdf6d7e43c4c51240dd20
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2024-08-01 10:56:05 -0400

    Remove unnecessary comment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { getChordFunctionFromName } = require('./chordFunctions'); // Ensure you have this module
+const { getChordFunctionFromName } = require('./chordFunctions'); 
 
 const app = express();
 const PORT = 8000;
